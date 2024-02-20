ee0a98306cbeeed44b583a7680da0e27be998161
Daniel <DanielDev2024@outlook.com>
2024-02-19 21:19:38 -0300

    first commit

---
+++ Models/PropertyAnotacaoModel.cs
@@ -0,0 +1,7 @@
+namespace AgendaVeicular.Models;
+public class PropertyAnotacaoModel
+{
+    public int IdAnotacao {get;set;}
+    public DateTime DataAtual  {get;set;}
+    public string? Registros {get;set;}
+}
